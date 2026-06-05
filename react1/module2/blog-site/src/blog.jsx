0e098454db5b6df3ef23f1064dd12c13c7601f31
Lokesh <lokeshkumar.patel@genpact.com>
2026-06-05 18:32:12 +0000

    Pushing module2 blog-site coding assignment

---
+++ react1/module2/blog-site/src/blog.jsx
@@ -0,0 +1,35 @@
+const blog = {
+  name: "Lokesh's Blog",
+  image:
+    "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=300",
+  about:
+    "Welcome to my blog where I share my thoughts on technology and life.",
+  posts: [
+    {
+      id: 1,
+      title: "My First Blog Post",
+      date: "June 5, 2026",
+      preview: "This is my first blog post built using React."
+    },
+    {
+      id: 2,
+      title: "Learning Components",
+      date: "June 6, 2026",
+      preview: "Today I learned how components work in React."
+    },
+    {
+      id: 3,
+      title: "Props are Awesome",
+      date: "June 7, 2026",
+      preview: "Props help us pass data from one component to another."
+    },
+    {
+      id: 4,
+      title: "Assignment Grade",
+      date: "June 9, 2026",
+      preview: "My props assignment was graded"
+    },
+  ]
+};
+
+export default blog;
