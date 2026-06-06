98af2c6cc2d2ccfc15f6e28ef89936ec36bf263a
Lokesh <lokeshkumar.patel@genpact.com>
2026-06-06 04:54:48 +0000

    Push the changes to git for fixing npm test

--- react1/module3/product-dashboard/src/components/ProductCard.jsx
+++ react1/module3/product-dashboard/src/components/ProductCard.jsx
@@ -1,25 +1,34 @@
 import styles from "../styles/ProductCard.module.css";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography, Button } from "@mui/material";
 
 function ProductCard(props) {
   return (
-    <Card
-      className={`${styles.card} ${
-        !props.product.inStock ? styles.outOfStock : ""
-      }`}
-    >
-      <CardContent>
+    <Card className={styles.card}>
+      <CardContent
+        className={
+          !props.product.inStock
+            ? "outOfStockClass"
+            : ""
+        }
+      >
         <Typography variant="h6">
           {props.product.name}
         </Typography>
 
         <Typography>
-          Price: ${props.product.price}
+          Price: {props.product.price}
         </Typography>
 
         <Typography>
           {props.product.inStock ? "In Stock" : "Out of Stock"}
         </Typography>
+
+        <Button
+          variant="outlined"
+          onClick={() => props.onRemove(props.product.id)}
+        >
+          Remove
+        </Button>
       </CardContent>
     </Card>
   );
